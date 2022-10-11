1dd8c96e955cc675e7f0f738b998f9246860cc84
Roman Baryshev <108750406+RomanBaryshevOG@users.noreply.github.com>
2022-10-11 19:45:22 +0500

    Added Tsk1.

--- tsk1.py
+++ tsk1.py
@@ -7,3 +7,49 @@
 # Все конфеты оппонента достаются сделавшему последний ход.
 # Сколько конфет нужно взять первому игроку,
 # чтобы забрать все конфеты у своего конкурента?
+
+
+import random
+
+messages = ['Ваша очередь ', 'возьмите конфеты', 
+            'сколько?', 'Next Move', 'Ваш ход']
+
+
+def play_game(n, m, players, messages):
+    count = 0
+    if n%10 == 1 and 9 > n > 10: letter = 'а'
+    elif 1 < n%10 < 5 and 9 > n > 10: letter = 'ы'
+    else: letter = ''
+    while n > 0:
+        print(f'{players[count%2]}, {random.choice(messages)}')
+        move = int(input())
+        if move > n or move > m:
+            print(f'Это слишком много, можно взять не более {m} конфет{letter}, у нас всего {n} конфет{letter}')
+            attempt = 3
+            while attempt > 0:
+                if n >= move <= m:
+                    break
+                print(f'Попробуйте ещё раз, у Вас {attempt} попытки')
+                move = int(input())
+                attempt -=1
+            else: 
+                return print(f'Game over!')
+        n = n - move
+        if n > 0: print(f'Осталось {n} конфет{letter}')
+        else: print('Все конфеты разобраны.')
+        count +=1
+    return players[not count%2]
+
+
+
+player1 = input('First player name:\n')
+player2 = input('Second player name:\n')
+players = [player1, player2]
+
+n = int(input('На сколько играем???\n '))
+m = int(input('Сколько максимально будем брать конфет за один ход?\n '))
+
+winer = play_game(n, m, players, messages)
+if not winer:
+    print('.....увы......')
+else: print(f'Победил {winer}!!!!')
